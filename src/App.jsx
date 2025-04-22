9218c75aa5631a3267ef53a8a8cab8a05e52e781
Kittypoda <odakrolstad@gmail.com>
2025-04-22 12:05:13 +0200

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-function App() {
-  return <h1 className="text-2xl text-red-500 font-bold">Hello Holidaze 🌴</h1>
-}
+import { Outlet, Link } from 'react-router-dom';
 
-export default App
+export default function App() {
+  return (
+    <div>
+      <nav className="bg-gray-100 p-4 flex gap-4">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/avenue">Avenue</Link>
+      </nav>
+
+      {/* Her vises innholdet fra Home/Login/Register osv. */}
+      <main className="p-6">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Home from './pages/home';
+import Login from './pages/Login';
+import './index.css';
+import Register from './pages/Register';
+import Avenue from './pages/AvenuePage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />}
+           />
+            <Route path="avenue" element={<Avenue />}
+           />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
