68f7d062efe2fe70977668f1263939dd8f179759
Kittypoda <odakrolstad@gmail.com>
2025-05-23 15:53:22 +0200

    Renaming bc conflict with netlify

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Home from './pages/home';
+import Home from './pages/HomeTemp';
 import Login from './pages/Login';
 import './index.css';
 import Register from './pages/Register';
