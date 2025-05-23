6eba6329be865bbbf0fa9ac7b720e1e6aab4586b
Kittypoda <odakrolstad@gmail.com>
2025-05-23 15:53:59 +0200

    Renaming again

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Home from './pages/HomeTemp';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import './index.css';
 import Register from './pages/Register';
